0ff732af64eb45671c80f5422074d4c837d70299
PXN011 <matheus.fernandes99@etec.sp.gov.br>
2023-06-06 19:36:42 -0300

    Fiz as questões que de acordo com o que compreendi.
    atividades de php e javascript.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Cabeçalho.php">
+    <title>Principal</title>
+</head>
+<body>
+    <div id="center">
+        <php href="Cabeçalho.php">
+
+        </php>
+
+        <php href="menu.php">
+            
+        </php>
+
+        <php href="rodape.php">
+
+        </php>
+    </div>
+
+
+
+    
+    
+    
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,165 @@
+<?php
+
+$menu = array{
+    => 'Rosa',
+            'cpf' =>class DataRequest {
+
+    private $DadosClientes = [
+        0 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cpf' => '555.333.888-77',
+            'email' => 'm@multidadosti.com.br'
+            'endereco' => 'Rua Veiga',
+            'nome' => 'Mendel',
+            'Relatório' => 'Produtos',
+            'telefone' => '(11) 98654-3333',
+            
+        ],
+        1 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cpf' => '333.555.777-88',
+            'email' => 'ig@multidadosti.com.br'
+            'endereco' => 'Rua Lopez',
+            'nome' => 'IG',
+            'Relatório' => 'Produtos',
+            'telefone' => '(11) 94002-8922',
+            
+        ],
+        2 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cpf' => '222.444.333-55',
+            'email' => 'arthur@multidadosti.com.br'
+            'endereco' => 'Rua Limoeiro',
+            'nome' => 'Arthur',
+            'Relatório' => 'Produtos',
+            'telefone' => '(11) 92233-4444',
+        ],
+        3 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'email' => 'frosa@multidadosti.com.br'
+            'endereco' => 'Rua Rangel',
+            'nome' => '111.777.222-33',
+            'Relatório' => 'Produtos',
+            'telefone' => '(11) 96685-2525',
+        ],
+        4 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cpf' => '333.111.222-99',
+            'email' => 'alves@multidadosti.com.br'
+            'endereco' => 'Rua Areias',
+            'nome' => 'Ant',
+            'Relatório' => 'Produtos',
+            'telefone' => '(11) 96868-5555',
+        ]
+    ];
+
+    private $DadosFornecedores = [
+        0 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cidade' => 'São Paulo',
+            'cpf' => '111.111.111-11',
+            'email' => 'm@gmail.com',
+            'nome' => 'Moisés',
+            'Relatório' => 'Produtos',
+        ],
+        1 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cidade' => 'Rio de Janeiro',
+            'cpf' => '222.222.222-22',
+            'email' => 'bras@gmail.com',
+            'nome' => 'Bras',
+            'Relatório' => 'Produtos',
+        ],
+        2 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cidade' => 'Bahia',
+            'cpf' => '333.333.333-33',
+            'email' => 'peixe@gmail.com',
+            'nome' => 'Peixoto',
+            'Relatório' => 'Produtos',
+            
+        ],
+        3 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cidade' => 'Santa Catarina',
+            'cpf' => '444.444.444-44',
+            'email' => 'um@gmail.com',
+            'nome' => 'Humberto',
+            'Relatório' => 'Produtos',
+        ],
+        4 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cidade' => 'Rio Grande do Sul',
+            'cpf' => '555.555.555-55',
+            'email' => 'costa@gmail.com',
+            'nome' => 'Costa',
+            'Relatório' => 'Produtos',
+        ]
+    ];
+
+    private $DadosUsuarios = [
+        0 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cpf' => '777.777.777-77',
+            'endereco' => 'Rua Agostinho',
+            'nome' => 'Souza',
+            'Relatório' => 'Produtos',
+            'telefone' => '(11) 92222-3333',
+            'usuario' => 'souza',
+        ],
+        1 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cpf' => '000.550.787-18',
+            'endereco' => 'Rua Gonzaga',
+            'nome' => 'Luiz',
+            'Relatório' => 'Produtos',
+            'telefone' => '(11) 91322-5922',
+            'usuario' => 'luizm',
+        ],
+        2 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cpf' => '223.411.353-58',
+            'endereco' => 'Rua Manu',
+            'nome' => 'Henrique',
+            'Relatório' => 'Produtos',
+            'telefone' => '(11) 92113-8989',
+            'usuario' => 'lhenrique',
+        ],
+        3 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cpf' => '113.765.282-22',
+            'endereco' => 'Rua Rangel',
+            'nome' => 'Astolfo',
+            'Relatório' => 'Produtos',
+            'telefone' => '(11) 96685-0001',
+            'usuario' => 'astff',
+        ],
+        4 => [
+            'Cadastro' => 'Perfil de acesso',
+            'Cadastro' => 'Produtos',
+            'cpf' => '332.151.287-33',
+            'endereco' => 'Rua Areias',
+            'nome' => 'Gomes',
+            'Relatório' => 'Produtos',
+            'telefone' => '(11) 96161-1616',
+            'usuario' => 'rgomes',
+        ]
+    ];
+
+    
+}
+};
